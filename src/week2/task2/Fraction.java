5169b67cff9521c23bd9a00b9f35eb112503cd02
NguyenTuanVu2105 <daigiachantran01@gmail.com>
2018-11-30 12:50:39 +0700

    Merge branch 'master' of C:\Users\Admin\Desktop\oop2018 with conflicts.

--- src/week2/task2/Fraction.java
+++ src/week2/task2/Fraction.java
@@ -1,82 +1,30 @@
 package week2.task2;
-import week2.task1.Task1;
 
 public class Fraction {
 
     // TODO: khai báo các thuộc tính
-    int numerator;
-    int denominator;
 
     public Fraction(int numerator, int denominator) {
         // TODO: khởi tạo giá trị cho các thuộc tính numberator (tử số), denominator (mẫu số)
-        this.denominator    = denominator;
-        this.numerator      = numerator;
     }
-
-    public void reduce(){
-        int gcd = Task1.gcd(numerator, denominator);
-        numerator = numerator/gcd;
-        denominator = denominator/gcd;
-    }
-
 
     public Fraction add(Fraction other) {
         // TODO: Phương thức cộng hai phân số (this và other), trả về đối tượng Fraction mới
-        other.reduce();
-        this.reduce();
-
-        int denominatorResult   = this.denominator * other.denominator / Task1.gcd(this.denominator, other.denominator); //lcd(this.denominator, other.denominator)
-        int numeratorResult     = this.numerator * (denominatorResult / this.denominator)
-                                + other.numerator * (denominatorResult / other.denominator);
-
-        return new Fraction(numeratorResult, denominatorResult);
+        return null;
     }
 
     public Fraction subtract(Fraction other) {
         // TODO: Phương thức trừ hai phân số (this và other), trả về đối tượng Fraction mới
-        other.reduce();
-        this.reduce();
-
-        int denominatorResult   = this.denominator * other.denominator / Task1.gcd(this.denominator, other.denominator); //lcd(this.denominator, other.denominator)
-        int numeratorResult     = this.numerator * (denominatorResult / this.denominator)
-                                - other.numerator * (denominatorResult / other.denominator);
-
-        return new Fraction(numeratorResult, denominatorResult);
+        return null;
     }
 
     public Fraction multiply(Fraction other) {
         // TODO: Phương thức nhân hai phân số (this và other), trả về đối tượng Fraction mới
-        other.reduce();
-        this.reduce();
-        int gcd1    = Task1.gcd(this.numerator,other.denominator);
-        int gcd2    = Task1.gcd(this.denominator,other.numerator);
-
-        int numeratorResult     = (this.numerator  /  gcd1)
-                                * (other.numerator /  gcd2);
-        int denominatorResult   = (this.denominator  / gcd2)
-                                * (other.denominator / gcd1);
-
-        return new Fraction(numeratorResult, denominatorResult);
+        return null;
     }
-
 
     public Fraction divide(Fraction other) {
         // TODO: Phương thức chia hai phân số (this và other), trả về đối tượng Fraction mới
-        other.reduce();
-        this.reduce();
-        int gcd1    = Task1.gcd(this.numerator,other.numerator);
-        int gcd2    = Task1.gcd(this.denominator,other.denominator);
-
-        int numeratorResult     = (this.numerator  /  gcd1)
-                                * (other.denominator /  gcd2);
-        int denominatorResult   = (this.denominator  / gcd2)
-                                * (other.numerator / gcd1);
-
-        return new Fraction(numeratorResult, denominatorResult);
-    }
-
-
-    public boolean equals(Object obj){
-        return (this.denominator == ((Fraction)obj).denominator) && (this.numerator == ((Fraction)obj).numerator);
+        return null;
     }
 }
